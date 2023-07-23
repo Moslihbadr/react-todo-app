4afb3819e9ba8d17727b89e1e54aea01b56a9f20
Badr <moslihbadr2@gmail.com>
2023-07-24 00:58:07 +0100

    add style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
+
 function App() {
   return (
-    <div className="container">
-      test render
+    <div className='container p-2 text-center'>
+      <h1 className='m-3 mb-5'>Todo App</h1>
+      <TodoForm />
+      <TodoList />
     </div>
   );
 }
