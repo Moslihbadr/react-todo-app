a87cd2a5ece64cb812c391a641e4adbdb6741c7f
Badr <moslihbadr2@gmail.com>
2023-07-25 14:41:51 +0100

    update TodoList component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,7 +2,7 @@ import TodoItem from "./TodoItem";
 
 const TodoList = ({ tasks, onTaskClick, onTaskDelete }) => {
   return (
-    <div className="card" style={{width: '25rem'}}>
+    <div className="card" style={{width: '22rem'}}>
       <ul className="list-group list-group-flush">
         {tasks.map(task => <TodoItem key={task.id} task={task} onTaskClick={onTaskClick} onTaskDelete={onTaskDelete}/>)}
       </ul>
